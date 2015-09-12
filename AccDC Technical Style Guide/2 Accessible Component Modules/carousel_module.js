753b23c0baf0725864e49ab623468f7badcb7c47
Bryan Garaventa <bryan.garaventa@whatsock.com>
2015-09-12 11:55:25 -0700

    Updated the ARIA Carousel module to support Mac keystrokes on OS X

--- AccDC Technical Style Guide/2 Accessible Component Modules/carousel_module.js
+++ AccDC Technical Style Guide/2 Accessible Component Modules/carousel_module.js
@@ -1,6 +1,6 @@
 /*!
-Carousel From XML Module R2.3
-Copyright 2010-2014 Bryan Garaventa (WhatSock.com)
+Carousel From XML Module R2.4
+Copyright 2010-2015 Bryan Garaventa (WhatSock.com)
 Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under the terms of the Open Source Initiative OSI - MIT License
 */
 
@@ -215,7 +215,11 @@ Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under t
 											var aS = '';
 
 											if (!autoStart || typeof autoStart !== 'string')
-												aS = [0, 0];
+												aS =
+																[
+																0,
+																0
+																];
 
 											else
 												aS = autoStart.split(',');
@@ -358,7 +362,11 @@ Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under t
 									var ret = null;
 
 									if (handlers.btnPrev && typeof handlers.btnPrev === 'function' && dc.accDCObj)
-										ret = handlers.btnPrev.apply(this, [ev, dc]);
+										ret = handlers.btnPrev.apply(this,
+														[
+														ev,
+														dc
+														]);
 
 									if (typeof ret === 'boolean' && !ret)
 										return false;
@@ -406,7 +414,11 @@ Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under t
 									var ret = null;
 
 									if (handlers.btnNext && typeof handlers.btnNext === 'function' && dc.accDCObj)
-										ret = handlers.btnNext.apply(this, [ev, dc]);
+										ret = handlers.btnNext.apply(this,
+														[
+														ev,
+														dc
+														]);
 
 									if (typeof ret === 'boolean' && !ret)
 										return false;
@@ -454,7 +466,11 @@ Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under t
 									var ret = null;
 
 									if (handlers.btnPrevG && typeof handlers.btnPrevG === 'function')
-										ret = handlers.btnPrevG.apply(this, [ev, dc]);
+										ret = handlers.btnPrevG.apply(this,
+														[
+														ev,
+														dc
+														]);
 
 									if (typeof ret === 'boolean' && !ret)
 										return false;
@@ -479,7 +495,11 @@ Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under t
 									var ret = null;
 
 									if (handlers.btnNextG && typeof handlers.btnNextG === 'function')
-										ret = handlers.btnNextG.apply(this, [ev, dc]);
+										ret = handlers.btnNextG.apply(this,
+														[
+														ev,
+														dc
+														]);
 
 									if (typeof ret === 'boolean' && !ret)
 										return false;
@@ -501,14 +521,22 @@ Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under t
 												{
 												click: function(ev){
 													ev.preventDefault();
-													dc.pSlide.apply(this, [ev, dc]);
+													dc.pSlide.apply(this,
+																	[
+																	ev,
+																	dc
+																	]);
 												},
-												keypress: function(ev){
+												keydown: function(ev){
 													var k = ev.which || ev.keyCode;
 
 													if (k == 13 || k == 32){
 														ev.preventDefault();
-														dc.pSlide.apply(this, [ev, dc]);
+														dc.pSlide.apply(this,
+																		[
+																		ev,
+																		dc
+																		]);
 													}
 												}
 												});
@@ -517,14 +545,22 @@ Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under t
 												{
 												click: function(ev){
 													ev.preventDefault();
-													dc.nSlide.apply(this, [ev, dc]);
+													dc.nSlide.apply(this,
+																	[
+																	ev,
+																	dc
+																	]);
 												},
-												keypress: function(ev){
+												keydown: function(ev){
 													var k = ev.which || ev.keyCode;
 
 													if (k == 13 || k == 32){
 														ev.preventDefault();
-														dc.nSlide.apply(this, [ev, dc]);
+														dc.nSlide.apply(this,
+																		[
+																		ev,
+																		dc
+																		]);
 													}
 												}
 												});
@@ -534,14 +570,22 @@ Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under t
 													{
 													click: function(ev){
 														ev.preventDefault();
-														dc.pGroup.apply(this, [ev, dc]);
+														dc.pGroup.apply(this,
+																		[
+																		ev,
+																		dc
+																		]);
 													},
-													keypress: function(ev){
+													keydown: function(ev){
 														var k = ev.which || ev.keyCode;
 
 														if (k == 13 || k == 32){
 															ev.preventDefault();
-															dc.pGroup.apply(this, [ev, dc]);
+															dc.pGroup.apply(this,
+																			[
+																			ev,
+																			dc
+																			]);
 														}
 													}
 													});
@@ -550,21 +594,29 @@ Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under t
 													{
 													click: function(ev){
 														ev.preventDefault();
-														dc.nGroup.apply(this, [ev, dc]);
+														dc.nGroup.apply(this,
+																		[
+																		ev,
+																		dc
+																		]);
 													},
-													keypress: function(ev){
+													keydown: function(ev){
 														var k = ev.which || ev.keyCode;
 
 														if (k == 13 || k == 32){
 															ev.preventDefault();
-															dc.nGroup.apply(this, [ev, dc]);
+															dc.nGroup.apply(this,
+																			[
+																			ev,
+																			dc
+																			]);
 														}
 													}
 													});
 								}
 
 								if (vals.timer > 0){
-									$A.bind('body', 'keypress.' + bId, function(ev){
+									$A.bind('body', 'keydown.' + bId, function(ev){
 										var k = ev.which || ev.keyCode;
 
 										if (k == 27 && !paused){
@@ -689,7 +741,11 @@ Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under t
 				}
 
 				if (config.step)
-					config.step.apply(ele, [nw, nw2]);
+					config.step.apply(ele,
+									[
+									nw,
+									nw2
+									]);
 			}
 
 			else{
@@ -702,7 +758,11 @@ Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under t
 								$A.css(ele2, t, targ2[t]);
 
 				if (config.complete)
-					config.complete.apply(ele, [targ, targ2]);
+					config.complete.apply(ele,
+									[
+									targ,
+									targ2
+									]);
 			}
 		}, 10);
 	};
