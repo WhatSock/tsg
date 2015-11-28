6299004de32873d829070ab0dcac7fe927070b24
Bryan Garaventa <bryan.garaventa@whatsock.com>
2015-11-27 18:39:54 -0800

    Updated the ProgressBar module to use ARIA named regions to bring it more into alignment with ARIA 1.1

--- AccDC Technical Style Guide/2 Accessible Component Modules/progress_bar.js
+++ AccDC Technical Style Guide/2 Accessible Component Modules/progress_bar.js
@@ -1,6 +1,6 @@
 /*!
-Progress Bar Module R1.0
-Copyright 2010-2013 Bryan Garaventa (WhatSock.com)
+Progress Bar Module R1.1
+Copyright 2010-2015 Bryan Garaventa (WhatSock.com)
 Part of AccDC, a Cross-Browser JavaScript accessibility API, distributed under the terms of the Open Source Initiative OSI - MIT License
 	*/
 
@@ -26,6 +26,24 @@ $A.createEl('progress');
 						accEnd: 'End',
 						showHiddenClose: false,
 						allowCascade: true,
+						runDuring: function(dc){
+							$A.setAttr(dc.accDCObj,
+											{
+											role: 'region',
+											'aria-label': dc.role
+											});
+
+							dc.fn.sraStart.innerHTML = dc.fn.sraEnd.innerHTML = '';
+							$A.setAttr(dc.fn.sraStart,
+											{
+											'aria-hidden': 'true'
+											});
+
+							$A.setAttr(dc.fn.sraEnd,
+											{
+											'aria-hidden': 'true'
+											});
+						},
 						runBefore: function(dc){
 							dc.source = $A.createEl('progress',
 											{
