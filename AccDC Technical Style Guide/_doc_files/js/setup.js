fdc1f8bc336b23b2ef3d3ae3fbb08c639e97d091
Bryan Garaventa <bryan.garaventa@whatsock.com>
2020-02-24 13:10:42 -0800

    Updated landing page setup file to support auto-expanding of module sections.

--- AccDC Technical Style Guide/_doc_files/js/setup.js
+++ AccDC Technical Style Guide/_doc_files/js/setup.js
@@ -1,177 +1,162 @@
-(function(){
+(function() {
+  $A.bind(window, "load", function() {
+    var apiToggles = [],
+      moduleToggles = [],
+      ariaToggles = [],
+      eventToggles = [];
 
-	$A.bind(window, 'load', function(){
-		var apiToggles = [], moduleToggles = [], ariaToggles = [], eventToggles = [];
+    $A.query("div.AccDC .panel > .control > button", function(i, o) {
+      $A.setAttr(o, "aria-expanded", "false");
+      apiToggles.push(
+        new $A.Toggle(o, {
+          state: false,
+          suppressARIAPressed: true,
+          callback: function(state) {
+            $A.setAttr(this, "aria-expanded", state ? "true" : "false");
 
-		$A.query('div.AccDC .panel > .control > button', function(i, o){
-			$A.setAttr(o, 'aria-expanded', 'false');
-			apiToggles.push(new $A.Toggle(o,
-							{
-							state: false,
-							suppressARIAPressed: true,
-							callback: function(state){
-								$A.setAttr(this, 'aria-expanded', state ? 'true' : 'false');
+            if (state) {
+              $A.addClass(this, "pressed");
+              $A.remClass(this.parentNode.nextElementSibling, "hdn");
+            } else {
+              $A.remClass(this, "pressed");
+              $A.addClass(this.parentNode.nextElementSibling, "hdn");
+            }
+            return true;
+          }
+        })
+      );
+    });
 
-								if (state){
-									$A.addClass(this, 'pressed');
-									$A.remClass(this.parentNode.nextElementSibling, 'hdn');
-								}
+    var moduleMap = {};
 
-								else{
-									$A.remClass(this, 'pressed');
-									$A.addClass(this.parentNode.nextElementSibling, 'hdn');
-								}
-								return true;
-							}
-							}));
-		});
+    $A.query("div.Modules .panel > .control > button", function(i, o) {
+      $A.setAttr(o, "aria-expanded", "false");
+      moduleMap[o.id] = new $A.Toggle(o, {
+        state: false,
+        suppressARIAPressed: true,
+        callback: function(state) {
+          $A.setAttr(this, "aria-expanded", state ? "true" : "false");
 
-		$A.query('div.Modules .panel > .control > button', function(i, o){
-			$A.setAttr(o, 'aria-expanded', 'false');
-			moduleToggles.push(new $A.Toggle(o,
-							{
-							state: false,
-							suppressARIAPressed: true,
-							callback: function(state){
-								$A.setAttr(this, 'aria-expanded', state ? 'true' : 'false');
+          if (state) {
+            $A.addClass(this, "pressed");
+            $A.remClass(this.parentNode.nextElementSibling, "hdn");
+          } else {
+            $A.remClass(this, "pressed");
+            $A.addClass(this.parentNode.nextElementSibling, "hdn");
+          }
+          return true;
+        }
+      });
+      moduleToggles.push(moduleMap[o.id]);
+    });
 
-								if (state){
-									$A.addClass(this, 'pressed');
-									$A.remClass(this.parentNode.nextElementSibling, 'hdn');
-								}
+    $A.query("div.ARIA .panel > .control > button", function(i, o) {
+      $A.setAttr(o, "aria-expanded", "false");
+      ariaToggles.push(
+        new $A.Toggle(o, {
+          state: false,
+          suppressARIAPressed: true,
+          callback: function(state) {
+            $A.setAttr(this, "aria-expanded", state ? "true" : "false");
 
-								else{
-									$A.remClass(this, 'pressed');
-									$A.addClass(this.parentNode.nextElementSibling, 'hdn');
-								}
-								return true;
-							}
-							}));
-		});
+            if (state) {
+              $A.addClass(this, "pressed");
+              $A.remClass(this.parentNode.nextElementSibling, "hdn");
+            } else {
+              $A.remClass(this, "pressed");
+              $A.addClass(this.parentNode.nextElementSibling, "hdn");
+            }
+            return true;
+          }
+        })
+      );
+    });
 
-		$A.query('div.ARIA .panel > .control > button', function(i, o){
-			$A.setAttr(o, 'aria-expanded', 'false');
-			ariaToggles.push(new $A.Toggle(o,
-							{
-							state: false,
-							suppressARIAPressed: true,
-							callback: function(state){
-								$A.setAttr(this, 'aria-expanded', state ? 'true' : 'false');
+    $A.query("div.Events .panel > .control > button", function(i, o) {
+      $A.setAttr(o, "aria-expanded", "false");
+      eventToggles.push(
+        new $A.Toggle(o, {
+          state: false,
+          suppressARIAPressed: true,
+          callback: function(state) {
+            $A.setAttr(this, "aria-expanded", state ? "true" : "false");
 
-								if (state){
-									$A.addClass(this, 'pressed');
-									$A.remClass(this.parentNode.nextElementSibling, 'hdn');
-								}
+            if (state) {
+              $A.addClass(this, "pressed");
+              $A.remClass(this.parentNode.nextElementSibling, "hdn");
+            } else {
+              $A.remClass(this, "pressed");
+              $A.addClass(this.parentNode.nextElementSibling, "hdn");
+            }
+            return true;
+          }
+        })
+      );
+    });
 
-								else{
-									$A.remClass(this, 'pressed');
-									$A.addClass(this.parentNode.nextElementSibling, 'hdn');
-								}
-								return true;
-							}
-							}));
-		});
+    $A.bind("div.expand-collapse > p > button", "click", function(ev) {
+      var o = this,
+        a = null,
+        c = false;
 
-		$A.query('div.Events .panel > .control > button', function(i, o){
-			$A.setAttr(o, 'aria-expanded', 'false');
-			eventToggles.push(new $A.Toggle(o,
-							{
-							state: false,
-							suppressARIAPressed: true,
-							callback: function(state){
-								$A.setAttr(this, 'aria-expanded', state ? 'true' : 'false');
+      if (o.id == "col-1") {
+        a = apiToggles;
+        c = true;
+      } else if (o.id == "exp-1") {
+        a = apiToggles;
+        c = false;
+      } else if (o.id == "col-2") {
+        a = moduleToggles;
+        c = true;
+      } else if (o.id == "exp-2") {
+        a = moduleToggles;
+        c = false;
+      } else if (o.id == "col-3") {
+        a = ariaToggles;
+        c = true;
+      } else if (o.id == "exp-3") {
+        a = ariaToggles;
+        c = false;
+      } else if (o.id == "col-4") {
+        a = eventToggles;
+        c = true;
+      } else if (o.id == "exp-4") {
+        a = eventToggles;
+        c = false;
+      }
 
-								if (state){
-									$A.addClass(this, 'pressed');
-									$A.remClass(this.parentNode.nextElementSibling, 'hdn');
-								}
+      for (var i = 0; i < a.length; i++) a[i].set(c ? false : true);
+      $A.announce(c ? "Collapsed" : "Expanded");
+      ev.preventDefault();
+    });
 
-								else{
-									$A.remClass(this, 'pressed');
-									$A.addClass(this.parentNode.nextElementSibling, 'hdn');
-								}
-								return true;
-							}
-							}));
-		});
+    $A.bind("p.topLink a", "click", function(ev) {
+      $A.setFocus($A.query("h1")[0]);
+      ev.preventDefault();
+    });
 
-		$A.bind('div.expand-collapse > p > button', 'click', function(ev){
-			var o = this, a = null, c = false;
+    // Setup skip link functionality
+    $A.bind("#skipLink", "click", function(ev) {
+      $A.setFocus($A.getEl("wrapper"));
+      ev.preventDefault();
+    });
 
-			if (o.id == 'col-1'){
-				a = apiToggles;
-				c = true;
-			}
+    $A.bind("#mlto", {
+      click: function(ev) {
+        this.href = "mailto:bryan.garaventa@whatsock.com";
+      },
+      blur: function(ev) {
+        this.href = "#";
+      }
+    });
 
-			else if (o.id == 'exp-1'){
-				a = apiToggles;
-				c = false;
-			}
+    createHeaderNav();
 
-			else if (o.id == 'col-2'){
-				a = moduleToggles;
-				c = true;
-			}
+    SyntaxHighlighter.defaults["gutter"] = false;
+    SyntaxHighlighter.all();
 
-			else if (o.id == 'exp-2'){
-				a = moduleToggles;
-				c = false;
-			}
-
-			else if (o.id == 'col-3'){
-				a = ariaToggles;
-				c = true;
-			}
-
-			else if (o.id == 'exp-3'){
-				a = ariaToggles;
-				c = false;
-			}
-
-			else if (o.id == 'col-4'){
-				a = eventToggles;
-				c = true;
-			}
-
-			else if (o.id == 'exp-4'){
-				a = eventToggles;
-				c = false;
-			}
-
-			for (var i = 0; i < a.length; i++)
-							a[i].set(c ? false : true);
-			$A.announce(c ? 'Collapsed' : 'Expanded');
-			ev.preventDefault();
-		});
-
-		$A.bind('p.topLink a', 'click', function(ev){
-			$A.setFocus($A.query('h1')[0]);
-			ev.preventDefault();
-		});
-
-		// Setup skip link functionality
-		$A.bind('#skipLink', 'click', function(ev){
-			$A.setFocus($A.getEl('wrapper'));
-			ev.preventDefault();
-		});
-
-		$A.bind('#mlto',
-						{
-						click: function(ev){
-							this.href = 'mailto:bryan.garaventa@whatsock.com';
-						},
-						blur: function(ev){
-							this.href = '#';
-						}
-						});
-
-		createHeaderNav();
-
-		SyntaxHighlighter.defaults['gutter'] = false;
-		SyntaxHighlighter.all();
-
-		// Generate permalinks
-		/*var baseURL = 'http://whatsock.com/tsg/#';
+    // Generate permalinks
+    /*var baseURL = 'http://whatsock.com/tsg/#';
 		$A.query('h2', function(i, o){
 			var d = o.parentNode, a = $A.createEl('a',
 							{
@@ -185,44 +170,59 @@
 			$A.css(a, 'left', -(a.offsetWidth));
 		});*/
 
-		var verNode = $A.getEl('AccDCCurrentVerS1');
+    var verNode = $A.getEl("AccDCCurrentVerS1");
 
-		if (verNode && $A.version){
-			verNode.innerHTML = ('Currently running AccDC API for Standalone, version: ' + $A.version).announce();
-		}
+    if (verNode && $A.version) {
+      verNode.innerHTML = (
+        "Currently running AccDC API for Standalone, version: " + $A.version
+      ).announce();
+    }
 
-		if (window.navigator.onLine)
-			// Check for updates
-			$A.getScript('http://api.whatsock.com/tsg-updates.js');
-	});
+    var loc = top.location.href || "",
+      hsh = loc.slice(loc.indexOf("tgl-2-"));
+    if (moduleMap[hsh]) {
+      moduleMap[hsh].set(true);
+    }
 
-	var hds = {}, createHeaderNav = function(){
-		var ph = $A.getEl('skipLinks'), hs = $A.query('div.hd > h2');
-		hds = {};
+    if (window.navigator.onLine)
+      // Check for updates
+      $A.getScript("http://api.whatsock.com/tsg-updates.js");
+  });
 
-		for (var i = 0; i < hs.length; i++){
-			var h = hs[i];
+  var hds = {},
+    createHeaderNav = function() {
+      var ph = $A.getEl("skipLinks"),
+        hs = $A.query("div.hd > h2");
+      hds = {};
 
-			if (ph && h.className !== 'skip'){
-				h.id = 'H' + $A.genId();
-				var leli = $A.createEl('li');
-				var a = $A.createEl('a',
-								{
-								href: '#'
-								}, null, h.id, document.createTextNode($A.getText(h)));
+      for (var i = 0; i < hs.length; i++) {
+        var h = hs[i];
 
-				ph.appendChild(leli);
-				leli.appendChild(a);
-				$A.setAttr(h, 'tabindex', -1);
-				hds[h.id] = h;
-				$A.bind(a, 'click', function(ev){
-					hds[this.className].focus();
-					ev.preventDefault();
-				});
+        if (ph && h.className !== "skip") {
+          h.id = "H" + $A.genId();
+          var leli = $A.createEl("li");
+          var a = $A.createEl(
+            "a",
+            {
+              href: "#"
+            },
+            null,
+            h.id,
+            document.createTextNode($A.getText(h))
+          );
 
-			//if (i < (hs.length - 1))
-			//ph.appendChild($A.createEl('span', null, null, null, document.createTextNode(' | ')));
-			}
-		}
-	};
+          ph.appendChild(leli);
+          leli.appendChild(a);
+          $A.setAttr(h, "tabindex", -1);
+          hds[h.id] = h;
+          $A.bind(a, "click", function(ev) {
+            hds[this.className].focus();
+            ev.preventDefault();
+          });
+
+          //if (i < (hs.length - 1))
+          //ph.appendChild($A.createEl('span', null, null, null, document.createTextNode(' | ')));
+        }
+      }
+    };
 })();
